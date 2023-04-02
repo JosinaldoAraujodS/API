6e96dcf52070402649532cc4cacb309f4b6f2c65
JosinaldoAraujodS <113636538+JosinaldoAraujodS@users.noreply.github.com>
2023-04-02 13:21:04 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    return "API IN THE AIR!"
+
+
+@app.get("/ident/<name>")
+def ident(name):
+    return f"opa {name}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
